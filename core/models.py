7617e0a5ada1e6aaae15604fcea7a439b0e1b60c
Omona Emmanuel <omonaemman@gmail.com>
2024-10-16 11:24:10 +0300

    edited some files

--- core/models.py
+++ core/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+# User model
+class User(AbstractUser):
+    # TODO: add other fields
+    pass
